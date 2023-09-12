d15bd3f7b9313f5d6390bbf99bd0502bf6ab2934
Mehdi Ghorbani <ghorbanimahdi565@gmail.com>
2023-09-12 21:22:36 +0200

    -Change video download structure

--- video_preprocessing/videoDownloader.py
+++ video_preprocessing/videoDownloader.py
@@ -6,6 +6,7 @@ python3 your_script.py /path/to/your.csv /path/to/save/videos 10
 
 import argparse
 import os
+import yaml
 import sys
 sys.path.append('../helper')
 from dataLoader import *
@@ -13,10 +14,13 @@ from dataLoader import *
 base_directory = '../original_videos'
 base_data_dir = '../data/'
 
+with open('../data/track.yaml', 'w') as yaml_file:
+    data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+total_number_of_videos = data['total_videos_checkpoint']
+
 def main():
     parser = argparse.ArgumentParser(description='Download videos from CSV URLs')
     parser.add_argument('csv_file_name', help='Path to the CSV file containing video URLs')
-    parser.add_argument('iteration', help='Number of iterations for downloading videos')
     parser.add_argument('num_videos', type=int, help='Number of videos per iteration')
 
     args = parser.parse_args()
@@ -31,30 +35,22 @@ def main():
     for directory in directories:
         print(directory)
 
-    for _ in range(int(args.iteration)):
 
-        indexes = []
-        total_number_of_videos = 0
-        # Iterate through the strings and extract the numeric part
-        for name in directories:
-            parts = name.split("_")
+    createDirectory(f"../original_videos/video_{args.num_videos}")
 
-            if parts[0].isdigit():
-                indexes.append(int(parts[0]))
-            if parts[2].isdigit():
-                total_number_of_videos += int(parts[2])
+    rows = read_data(base_data_dir + args.csv_file_name, start_index=total_number_of_videos, end_index=args.num_videos + total_number_of_videos)
+    video_urls = []
 
+    for index in rows:
+        video_urls.append(index[1])
 
-        createDirectory(f"../original_videos/{len(indexes)}_video_{args.num_videos}")
+    indexes_to_delete = download_videos(video_urls, base_directory+f"/{len(indexes)}_video_{args.num_videos}")
 
+#Update the yaml file
+for i in indexes_to_delete:
+    data['failed_indexes'].append(i)
 
-        rows = read_data(base_data_dir + args.csv_file_name, start_index=total_number_of_videos, end_index=args.num_videos + total_number_of_videos)
-        video_urls = []
-
-        for index in rows:
-            video_urls.append(index[1])
-
-        indexes_to_delete = download_videos(video_urls, base_directory+f"/{len(indexes)}_video_{args.num_videos}")
+data['failed_indexes'] += total_videos_checkpoint
 
 if __name__ == "__main__":
     main()
