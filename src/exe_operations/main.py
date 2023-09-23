dc89650942b5bf73a7be155e375bb3dd983bf055
Mehdi Ghorbani <ghorbanimahdi565@gmail.com>
2023-09-23 22:01:19 +0200

    -Add Tensor loading section

--- src/exe_operations/main.py
+++ src/exe_operations/main.py
@@ -4,7 +4,7 @@ from video_diffusion_pytorch import Unet3D, GaussianDiffusion
 from tqdm import trange
 sys.path.append('helper')
 from dataLoader import read_data
-#from trainingCheckPoint import getLatestCheckpoint
+from trainingCheckPoint import getLatestCheckpoint
 sys.path.append('tensorMaker')
 from saveTensor import loadTensor
 sys.path.append('common')
@@ -18,34 +18,51 @@ def main():
     parser.add_argument('csv_file_path', help='Path to the CSV file containing video descriptions')
     args = parser.parse_args()
 
-    '''Read Training Data'''
+    # TODO Keep main.py only for training and move the following lines to a separate directory
+    '''Read Training Text Data'''
     training_rows = read_data(os.path.join(paths.base_data_dir + '/csv_files/customised', args.csv_file_path))
-    training_video_description = []
+    train_text = []
     # Read video descriptions
     for index in training_rows:
-        training_video_description.append(index[1])
+        train_text.append(index[1])
     # Delete for memory efficiency purposes
     del training_rows
     # garbage collector (Test if this command really helps improve memory efficiency)
 
-    '''Read Validation Data'''
+    '''Read Validation Text Data'''
     validation_rows = read_data(os.path.join(paths.base_data_dir + '/csv_files/customised', args.csv_file_path.replace("_train", "_val")))
-    val_video_description = []
+    val_text = []
     # Read video descriptions
     for index in validation_rows:
-        val_video_description.append(index[1])
+        val_text.append(index[1])
     # Delete for memory efficiency purposes
     del validation_rows
     
     # garbage collector (Test if this command really helps improve memory efficiency)
     gc.collect()
 
-    #torch.cuda.empty_cache()
+    '''Load Training Tensor'''
+    last_checkpoint = getLatestCheckpoint(os.path.join(paths.training_checkpoint_dir, args.csv_file_path.replace("_train.csv")))
+    FolderPath = f'/{args.csv_file_path.replace(".csv", "")}'
+    train_tensor = loadTensor(os.path.join(paths.tensor_path + FolderPath, f'track_{last_checkpoint}.pt'))
+
+    '''Load Validation Tensor'''
+    # Assuming there is only one Tensor for Validation (If running out of Memory, simply devide the Tensor in smaller Tnsors and randomly choose one of them)
+    FolderPath = f'/{args.csv_file_path.replace("_train.csv", "_val")}'
+    val_tensor = loadTensor(os.path.join(paths.tensor_path + FolderPath, 'track_0.pt'))
+    print("\nValidation Tensor has been loaded successfully!!\n")
+
+    '''Model'''
     model = Unet3D(
         dim=64,
         use_bert_text_cond=True,  # this must be set to True to auto-use the bert model dimensions
         dim_mults=(1, 2, 4, 8),
     )
+
+    # Load the latest model
+    if last_checkpoint != 0: 
+        model.load_state_dict(torch.load('my_model.pth'))
+        model.train()  # Set the model in training mode
 
     diffusion = GaussianDiffusion(
         model,
@@ -54,13 +71,6 @@ def main():
         timesteps=1000,   # number of steps
         loss_type='l1'    # L1 or L2
     )
-
-    '''Load Validation Tensor'''
-    # Assuming there is only one Tensor for Validation (If running out of Memory, simply devide the Tensor in smaller Tnsors and randomly choose one of them)
-    FolderPath = f'/{args.csv_file_path.replace("_train.csv", "_val")}'
-    val_tensors = loadTensor(os.path.join(paths.tensor_path + FolderPath, 'track_0.pt'))
-    val_text = ["text"] * 30
-    print("\nValidation Tensor has been loaded successfully!!\n")
 
     # Assuming you have your optimizer defined
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
@@ -78,8 +88,8 @@ def main():
         batch_indices = torch.randint(0, dataset_size, (batch_size,))
         
         # Sample a batch of training data
-        batch_videos = training_tensor[batch_indices]
-        batch_text = [video_description[idx] for idx in batch_indices]
+        batch_videos = train_tensor[batch_indices]
+        batch_text = [train_text[idx] for idx in batch_indices]
 
         # Forward pass with text conditioning
         loss = diffusion(batch_videos, cond=batch_text)
@@ -96,10 +106,10 @@ def main():
         # Validation every checkpoint_interval iterations
         if (iteration + 1) % checkpoint_interval == 0:
             val_loss = 0.0
-            num_val_batches = len(val_tensors) // batch_size
+            num_val_batches = len(val_tensor) // batch_size
 
-            for val_batch_start in range(0, len(val_tensors), batch_size):
-                val_batch_videos = val_tensors[val_batch_start:val_batch_start + batch_size]
+            for val_batch_start in range(0, len(val_tensor), batch_size):
+                val_batch_videos = val_tensor[val_batch_start:val_batch_start + batch_size]
                 val_batch_text = val_text[val_batch_start:val_batch_start + batch_size]
 
                 with torch.no_grad():
