16453fb3e70e3f5d72f13ba75b8bdc466c48799d
Mehdi Ghorbani <ghorbanimahdi565@gmail.com>
2023-09-30 20:23:22 +0200

    -Debug training checkpoint saving issue -Debug Model Loading issues -Add Excepiton handling to the model loading process

--- src/exe_operations/main.py
+++ src/exe_operations/main.py
@@ -57,18 +57,32 @@ def main():
         dim_mults=(1, 2, 4, 8),
     )
 
-    # Load the latest model
+    '''Load the latest model'''
     TRAINING_YAML = args.csv_file_path.replace("customised", "training").replace("_train.csv", ".yaml")
     last_checkpoint = getLatestTrainingCheckpoint(os.path.join(paths.TRAINING_CHECKPOINT_DIR + f'/{args.csv_file_path.replace("_train.csv", "")}', TRAINING_YAML))
-    if last_checkpoint != 0: 
-        model.load_state_dict(torch.load(os.path.join(paths.MODEL_DIR, 'main_model.pth')))
-        model.train()  # Set the model in training mode
+    if last_checkpoint != 0:
+        # Load the checkpoint
+        checkpoint = torch.load(os.path.join(paths.MODEL_DIR, 'main_model.pth'))
+        # Separate keys for model and optimizer
+        model_state_dict = checkpoint['model_state_dict']
+        # Check if the model state_dict is empty (indicating loading failure)
+        if not model_state_dict:
+            raise Exception("Failed to load the model state_dict.\nRuntime interuppted.")
+        # Load the model state_dict
+        model.load_state_dict(model_state_dict)
+        # Optional: Load other training-related information if needed
+        iteration = checkpoint['iteration']
+        optimizer_state_dict = checkpoint['optimizer_state_dict']
+        loss = checkpoint['loss']
+        val_loss = checkpoint['val_loss']
+        # Set the model in training mode
+        model.train()
 
     diffusion = GaussianDiffusion(
         model,
-        image_size=IMAGE_SIZE,    # height and width of frames
-        num_frames=NUM_FRAMES,    # number of video frames
-        timesteps=TIME_STEPS,   # number of steps
+        image_size=IMAGE_SIZE,          # height and width of frames
+        num_frames=NUM_FRAMES,          # number of video frames
+        timesteps=TIME_STEPS,           # number of steps
         loss_type=TRAINING_LOSS_TYPE    # L1 or L2
     )
 
@@ -139,7 +153,7 @@ def main():
     with open(os.path.join(paths.TRAINING_CHECKPOINT_DIR + f'/{args.csv_file_path.replace("_train.csv", "")}', TRAINING_YAML), "r") as f:
         yaml_data = yaml.load(f, Loader=yaml.FullLoader)
     yaml_data["Total_iterations"] = yaml_data["Total_iterations"] + 1
-    with open("my_yaml_file.yaml", "w") as f:
+    with open(os.path.join(paths.TRAINING_CHECKPOINT_DIR + f'/{args.csv_file_path.replace("_train.csv", "")}', TRAINING_YAML), "w") as f:
         yaml.dump(yaml_data, f)
 
     print(" \n'''Training finished'''\n ")
